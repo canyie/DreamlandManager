dd6ed1fc5f820054e30bf8330d3143e62658a1d6
command_block <mtf@ik.me>
2024-02-12 19:47:28 +0800

    refactor: format all UI strings using AutoCorrect (huacnlee/autocorrect) (#8)

--- app/src/main/java/top/canyie/dreamland/manager/troubleshoot/Alert.java
+++ app/src/main/java/top/canyie/dreamland/manager/troubleshoot/Alert.java
@@ -17,7 +17,7 @@ public enum Alert {
 
     MAGISK_TMP_UNREADABLE_0("Magisk tmpfs path not readable", "Magisk tmpfs 路径不可读"),
     MAGISK_TMP_UNREADABLE_1("Please disable Magisk Hide for Dreamland Manager", "请对梦境管理器禁用 Magisk Hide"),
-    MAGISK_TMP_UNREADABLE_2("or try upgrading Magisk to the latest canary version", "或尝试更新Magisk到最新的canary版本"),
+    MAGISK_TMP_UNREADABLE_2("or try upgrading Magisk to the latest canary version", "或尝试更新 Magisk 到最新的 canary 版本"),
 
     NO_RIRU_0("Riru is not installed", "Riru 未安装"),
     NO_RIRU_1("Please install Riru from Magisk app or https://github.com/RikkaApps/Riru/releases/", "请从 Magisk 或 https://github.com/RikkaApps/Riru/releases/ 安装 Riru"),
@@ -25,9 +25,9 @@ public enum Alert {
     NO_DREAMLAND_MODULE_0("Riru-Dreamland is not installed", "Riru-Dreamland 未安装"),
     NO_DREAMLAND_MODULE_1("Please install Riru-Dreamland from Dreamland Manager", "请从梦境管理器中安装 Riru-Dreamland"),
 
-    CORE_JAR_MISSING_0("Core jar file is missing", "核心jar文件已丢失"),
+    CORE_JAR_MISSING_0("Core jar file is missing", "核心 jar 文件已丢失"),
     CORE_JAR_MISSING_1("Please try re-installing Dreamland framework", "请尝试重新安装梦境框架"),
-    CORE_JAR_MISSING_2("and do not enable Magisk Hide for Dreamland Manager", "并对梦境管理器关闭Magisk Hide"),
+    CORE_JAR_MISSING_2("and do not enable Magisk Hide for Dreamland Manager", "并对梦境管理器关闭 Magisk Hide"),
 
     CONFIG_DIR_BROKEN_0("Config dir is missing or broken", "配置目录已丢失或存在问题"),
     CONFIG_DIR_BROKEN_1(CORE_JAR_MISSING_1),
@@ -35,19 +35,19 @@ public enum Alert {
     DISABLED_0("Dreamland framework is disabled", "梦境框架已被手动禁用"),
     DISABLED_1("Please delete the file /data/misc/dreamland/disable and restart the device", "请删除文件 /data/misc/dreamland/disable 并重新启动此设备"),
 
-    SEPOLICY_NOT_LOADED_0("SEPolicy patch rules not loaded properly by Magisk", "SEPolicy修补规则未被 Magisk 正确加载"),
+    SEPOLICY_NOT_LOADED_0("SEPolicy patch rules not loaded properly by Magisk", "SEPolicy 修补规则未被 Magisk 正确加载"),
     SEPOLICY_NOT_LOADED_1("Please try to reboot twice,", "请尝试重启至少两次，"),
-    SEPOLICY_NOT_LOADED_2("try upgrading Magisk to the latest canary version,", "更新Magisk到最新的canary版本，"),
-    SEPOLICY_NOT_LOADED_3("re-install Magisk, Riru and Dreamland", "重新安装Magisk，Riru和梦境框架"),
-    SEPOLICY_NOT_LOADED_4("or, report this problem to Magisk", "或将此问题报告给Magisk"),
+    SEPOLICY_NOT_LOADED_2("try upgrading Magisk to the latest canary version,", "更新 Magisk 到最新的 canary 版本，"),
+    SEPOLICY_NOT_LOADED_3("re-install Magisk, Riru and Dreamland", "重新安装 Magisk，Riru 和梦境框架"),
+    SEPOLICY_NOT_LOADED_4("or, report this problem to Magisk", "或将此问题报告给 Magisk"),
 
     WRONG_RIRU_SECONTEXT_0("Riru libs have wrong SELinux permission or context", "Riru 库存在错误的权限或 SELinux 上下文"),
     WRONG_RIRU_SECONTEXT_1("If re-install Riru and Dreamland cannot solve this problem, ", "如果重新安装 Riru 和梦境框架无法解决此问题，"),
-    WRONG_RIRU_SECONTEXT_2("then this is most likely due to your ROM maintainer added wrong SEPolicy rules", "那么这最有可能是因为您的ROM维护者添加了错误的SELinux规则"),
-    WRONG_RIRU_SECONTEXT_3("please ask your ROM maintainer not to do this or switch to other ROMs.", "请告诉该ROM维护者删除这些错误规则或选择其他ROM"),
+    WRONG_RIRU_SECONTEXT_2("then this is most likely due to your ROM maintainer added wrong SEPolicy rules", "那么这最有可能是因为您的 ROM 维护者添加了错误的 SELinux 规则"),
+    WRONG_RIRU_SECONTEXT_3("please ask your ROM maintainer not to do this or switch to other ROMs.", "请告诉该 ROM 维护者删除这些错误规则或选择其他 ROM"),
     WRONG_RIRU_SECONTEXT_4("For more information, please visit https://github.com/RikkaApps/Riru/issues/203", "有关更多信息，请访问 https://github.com/RikkaApps/Riru/issues/203"),
 
-    MAPLE_ENABLED_0("Huawei maple (maybe ark compiler's engine?) is enabled", "华为 maple （方舟编译器环境？）已启用"),
+    MAPLE_ENABLED_0("Huawei maple (maybe ark compiler's engine?) is enabled", "华为 maple（方舟编译器环境？）已启用"),
     MAPLE_ENABLED_1("This may cause Riru not work properly", "这可能会造成 Riru 工作异常"),
     MAPLE_ENABLED_2("Try upgrade to the latest Riru", "尝试更新到最新的 Riru"),
     MAPLE_ENABLED_3("or disable huawei maple by add ro.maple.enable=0", "或添加 ro.maple.enable=0 以禁用 maple"),
